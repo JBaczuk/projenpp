b55b08a08e229131bec210e1d17e80283c08b317
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2018-08-27 22:25:04 -0600

    add cli. Need to debug build

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,78 @@
 #include <iostream>
 #include <testfuncs.hpp>
+#include <boost/program_options.hpp>
 
 using namespace std;
+namespace po = boost::program_options;
 
-int main (void)
+int main (int argc, char* argv[])
 {
-    int answer = testfunc(1,1);
-    cout << "answer: " << answer << endl;
+	try {
+        bool some_flag_en;
+        string command;
+        vector<string> command_params;
+
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "Show this help message")
+            ("verbose,v", po::value<int>()->implicit_value(1),
+                  "Enable verbosity (optionally specify level)")
+            ("some_flag,t", po::bool_switch(&some_flag_en)->default_value(false),
+                  "Use some_flag configuration")
+            ("command", po::value< vector<string> >(), "Command")
+        ;
+
+        po::positional_options_description p;
+        p.add("command", -1);
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).
+                  options(desc).positional(p).run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << "Usage: options_description [options]\n";
+            std::cout << desc;
+            return 0;
+        }
+
+        if (vm.count("command"))
+        {
+            auto commands = vm["command"].as<vector<string>>();
+            command = commands[0];
+            vector<string> command_params_new(commands.begin() + 1, commands.end());
+            command_params = command_params_new;
+        }
+
+        if (vm.count("verbose")) {
+            std::cout << "Verbosity enabled.  Level is " << vm["verbose"].as<int>()
+                 << "\n";
+        }
+        
+        if (some_flag_en) {
+            std::cout << "Some flag enabled." << "\n";
+        }
+    
+        // Command Handler
+        if(command == "help")
+        {
+            std::cout << "Usage: options_description [options]\n";
+            std::cout << desc;
+            return 0;
+        }
+        else
+        {
+            std::cout << "Invalid command." << endl;
+            std::cout << "Usage: options_description [options]\n";
+            std::cout << desc;
+        }
+
+    }
+    catch(std::exception& e)
+    {
+        std::cout << e.what() << "\n";
+        return 1;
+    }
+
     return 0;
 }
-
